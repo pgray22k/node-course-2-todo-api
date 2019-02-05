c10b875c4c4009489370800967aa453a8460e13f
Philip <philip.k.gray@gmail.com>
2019-02-04 22:52:57 -0500

    Updated config settings for project

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,20 @@
 var env = process.env.NODE_ENV || 'development';
+
+if( env === 'development' || env === 'test') {
+    var config = require('./config.json');  //javascript can automatically load json files to become objects.
+    var envConfig = config[env]; // have to use bracket notation to get the property development or test
+
+    Object.keys(envConfig).forEach((key)=> {
+        process.env[key] = envConfig[key];  //will set PORT or MongoDB
+    });//takes an Object keys and turns them into array
+}
+
 console.log("env********", env);
 
-if ( env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI= 'mongodb://localhost:27017/TodoApp';
-} else if ( env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI= 'mongodb://localhost:27017/TodoAppTest';
-}
+// if ( env === 'development') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI= 'mongodb://localhost:27017/TodoApp';
+// } else if ( env === 'test') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI= 'mongodb://localhost:27017/TodoAppTest';
+// }
